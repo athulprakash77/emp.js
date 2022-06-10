c00db9a0ef28cefada335891b1877ae15bf8da7d
athulprakash007 <athull362@gmail.com>
2022-06-09 22:54:06 -0700

    employjs

---
+++ emp.js
@@ -0,0 +1,42 @@
+
+empbtnevent=()=>{
+ 
+      
+    employcode=(document.getElementById("employcode").value)
+    employname=(document.getElementById("employname").value)
+    companyname=(document.getElementById("companyname").value)
+    salary=(document.getElementById("salary").value)
+    emailid=(document.getElementById("emailid").value)
+    address=(document.getElementById("address").value)
+     phonenumber=(document.getElementById("phonenumber").value)
+     yearofexperience=(document.getElementById("yearofexperience").value)
+
+console.log(employcode)
+alert(employcode)
+
+console.log(employname)
+alert(employname)
+
+console.log(companyname)
+alert(companyname)
+
+console.log(salary) 
+alert(salary)
+ 
+
+console.log(emailid)
+alert(emailid)
+
+console.log(address)
+alert(address)
+
+
+console.log(phonenumber)
+alert(phonenumber)
+
+
+console.log(yearofexperience)
+alert(yearofexperience)
+
+
+}
